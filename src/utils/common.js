49fc863a959328b07a74487fc2143d20aa09132a
Nate247 <nathanielisewede@gmail.com>
2024-12-06 14:57:52 +0000

    common

--- src/utils/common.js
+++ src/utils/common.js
@@ -36,7 +36,7 @@ export const checkFavourites = (id, favourites = []) => {
     console.error("Favourites is not a valid array:", favourites);
     return "white"; // Return default if favourites is invalid
   }
-  return favourites.includes(id) ? "#fa3e5f" : "white";
+  return favourites?.includes(id) ? "#fa3e5f" : "white";
 };
 
 export const validateString = (value) => {
