1e69979a4b8b4f8e473949794a70c5e715e9e9dd
Nate247 <nathanielisewede@gmail.com>
2024-12-06 14:40:40 +0000

    update favourite list

--- src/utils/common.js
+++ src/utils/common.js
@@ -23,8 +23,8 @@ export const sliderSettings = {
   },
 };
 
-export const updateFavourites = (id, favourites) => {
-  if (favourites.includes(id)) {
+export const updateFavourites = (id, favourites = []) => {
+  if (Array.isArray(favourites) && favourites.includes(id)) {
     return favourites.filter((resId) => resId !== id);
   } else {
     return [...favourites, id];
